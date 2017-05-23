3809713ca584f796bfe745a992a91c38c58c7757
Oscar Duarte <leodufer@gmail.com>
2017-05-23 19:15:03 -0400

    commit inicial

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*
+*dato el proyecto catclicker declarar una variable cat 
+con los atributos name, photo, voteup, votedown
+asignar los valores del modelo datos a la estructura 
+html e implementar los eventos clik para el incremento 
+o decremento de los votos
+*/
