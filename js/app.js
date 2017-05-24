26ebdd85d78a8b2145a441011be2ee894a026120
Oscar Duarte <leodufer@gmail.com>
2017-05-23 20:41:03 -0400

    click only one cat

--- js/app.js
+++ js/app.js
@@ -5,3 +5,43 @@ asignar los valores del modelo datos a la estructura
 html e implementar los eventos clik para el incremento 
 o decremento de los votos
 */
+
+var cat = {
+	name : 'Ruano',
+	photo : 'img/cats/1.jpg',
+	voteup:10,
+	votedown:11
+}
+
+
+function init(){
+	showData(cat);
+	bindEvent();
+}
+
+function showData(cat){
+
+	$('h1').text(cat.name);
+	$('img').attr('src',cat.photo);
+	$('#up span').text(cat.voteup);
+	$('#down span').text(cat.votedown);
+}
+
+
+function clickUp(){
+	cat.voteup++;
+	$('#up span').text(cat.voteup);
+}
+
+function clickDown(){
+	cat.votedown++;
+	$('#down span').text(cat.votedown);
+}
+
+
+function bindEvent(){
+	$('#up').click(clickUp);
+	$('#down').click(clickDown);	
+}
+
+init();
